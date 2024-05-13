1a7efd3fc8acee8fdc89a39348e097fc33cac85f
Rodolfoovo <126284715+Rodolfoovo@users.noreply.github.com>
2024-05-13 11:33:44 -0300

    Criando as authenticações

---
+++ danielprojetodb3/src/main/java/com/example/danielprojetodb3/domain/dto/usuario/LoginResponseDTO.java
@@ -0,0 +1,21 @@
+package com.example.danielprojetodb3.domain.dto.usuario;
+
+public class LoginResponseDTO {
+    private String token;
+    private UsuarioResponseDTO usuarioResponseDTO;
+    
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public UsuarioResponseDTO getUsuarioResponseDTO() {
+        return usuarioResponseDTO;
+    }
+    public void setUsuarioResponseDTO(UsuarioResponseDTO usuarioResponseDTO) {
+        this.usuarioResponseDTO = usuarioResponseDTO;
+    }
+
+    
+}
